36ecd260c8e1dcfb6c8994a9cb383cc2177940fc
Andrew Zefanya <andrewz2002kapuas26@gmail.com>
2022-04-10 12:44:47 +0700

    finish migration camps, campbenefits, and checkout

---
+++ app/Models/CampBenefit.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Brick\Math\BigInteger;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CampBenefit extends Model
+{
+    use HasFactory;
+    $fillable = ['name'];
+  
+}
